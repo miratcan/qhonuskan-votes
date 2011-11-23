13b9b731ae30a96784718a2dba1f3add6e797e45
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2011-11-23 19:38:10 +0200

    not authenticated error

--- qhonuskan_votes/templatetags/votes.py
+++ qhonuskan_votes/templatetags/votes.py
@@ -4,8 +4,14 @@ register = template.Library()
 
 @register.filter
 def is_up_voted_by(object, user):
-    return bool(object.votes.filter(voter=user, value=1).count())
+    if user.is_authenticated():
+        return bool(object.votes.filter(voter=user, value=1).count())
+    else:
+        return False
 
 @register.filter
 def is_down_voted_by(object, user):
-    return bool(object.votes.filter(voter=user, value=-1).count())
+    if user.is_authenticated():
+        return bool(object.votes.filter(voter=user, value=-1).count())
+    else:
+        return False
