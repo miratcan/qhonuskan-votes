caca9297f00583f509f9b87f1df5c317572cc615
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2012-02-28 01:01:58 +0200

    reorder folder sturcture as described here: http://guide.python-distribute.org/quickstart.html

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name="qhonuskan-votes",
     url='https://github.com/miratcan/qhonuskan-votes',
-    license='BSD',
+    license='GPL',
     description="Easy to use reddit like voting system for django models.",
-    long_description=open('README.markdown', 'r').read(),
+    long_description=open('README.txt', 'r').read(),
     author='Mirat Can Bayrak',
     author_email='miratcanbayrak@gmail.com',
-    packages=find_packages(),
+    packages=['qhonuskan_votes', 'qhonuskan_votes.templatetags'],
+    package_data={'qhonuskan_votes': ['templates/*.html', 'static/*.css']},
+    version='0.1.0',
+    requires=['django(>=1.2)'],
     zip_safe=True,
     include_package_data=False,
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Operating System :: OS Independent',
@@ -19,4 +23,3 @@ setup(
         'Framework :: Django',
     ],
 )
-
