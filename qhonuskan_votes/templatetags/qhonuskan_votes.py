ded4d2a0a7b68344962364c8e1bfcea130ca903e
Ed Menendez <ed@menendez.com>
2012-04-05 22:06:13 -0700

    Added optional parameter to vote_buttons_for template tag to allow overriding of template.

--- qhonuskan_votes/templatetags/qhonuskan_votes.py
+++ qhonuskan_votes/templatetags/qhonuskan_votes.py
@@ -1,6 +1,6 @@
 from django import template
 from django.core.urlresolvers import reverse
-from django.template import RequestContext
+#from django.template import RequestContext
 
 register = template.Library()
 
@@ -8,6 +8,7 @@ register = template.Library()
 @register.inclusion_tag('voting_js.html')
 def voting_script():
     return {"vote_url": reverse("vote")}
+
 
 @register.filter
 def is_up_voted_by(object, user):
@@ -19,6 +20,7 @@ def is_up_voted_by(object, user):
     else:
         return False
 
+
 @register.filter
 def is_down_voted_by(object, user):
     """
@@ -29,11 +31,38 @@ def is_down_voted_by(object, user):
     else:
         return False
 
-@register.inclusion_tag('vote_buttons.html',takes_context=True)
-def vote_buttons_for(context, object):
-    return {
-        "user": context['user'],
-        "object": object,
-        "vote_model": "%s.%sVote" % (
-            object._meta.app_label, object._meta.object_name)
-    }
+
+@register.tag
+def vote_buttons_for(parser, token):
+    """
+    Takes two parameters. The first is the object the votes are for. And the second is
+    the template to use. By default it uses vote_buttons.html.
+
+    Usage::
+        {% vote_buttons_for idea %}
+        {% vote_buttons_for idea "app/follow_form.html" %}
+    """
+    token_contents = token.split_contents()
+    obj = token_contents[1]
+    if len(token_contents) > 2:
+        template_loc = token_contents[2]
+    else:
+        template_loc = 'vote_buttons.html'
+    return VoteButtonsNode(obj, template_loc)
+
+
+class VoteButtonsNode(template.Node):
+    def __init__(self, obj, template_loc):
+        self.obj = obj
+        self.template_loc = template_loc
+
+    def render(self, context):
+        t = template.loader.get_template(self.template_loc)
+        obj = context[self.obj]
+        c = {
+            "user": context['user'],
+            "object": obj,
+            "vote_model": "%s.%sVote" % (
+                obj._meta.app_label, obj._meta.object_name)
+        }
+        return t.render(template.Context(c, autoescape=context.autoescape))
