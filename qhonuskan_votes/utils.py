f8c956c8f26d3ae5175d9bb9d24fcfddc7ff1206
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2013-03-09 01:40:50 +0200

    pep8 cleanup for utils.py

--- qhonuskan_votes/utils.py
+++ qhonuskan_votes/utils.py
@@ -2,14 +2,17 @@ from qhonuskan_votes.models import _vote_models
 from django.db.models import aggregates
 from django.db.models.sql import aggregates as sql_aggregates
 
+
 def get_vote_model(model_name):
     if model_name in _vote_models:
         return _vote_models[model_name]
     else:
         raise Exception('No such vote model "%s"' % model_name)
 
+
 class SumWithDefault(aggregates.Aggregate):
     name = 'SumWithDefault'
+
 
 class SQLSumWithDefault(sql_aggregates.Sum):
     sql_template = 'COALESCE(%(function)s(%(field)s), %(default)s)'
