f8c169b2e871412d9266a22bef95ea7a7985b89a
Gokmen Gorgen <gokmen@alageek.com>
2013-05-01 21:48:00 +0300

    use compat module for fixing lettuce error. that's why I don't know =(

--- demo/urls.py
+++ demo/urls.py
@@ -1,14 +1,13 @@
-try:
-    from django.conf.urls import patterns, include, url
-except:
-    from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from qhonuskan_votes.compat import patterns, include, url
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^$', 'demo.views.home', name='home'),
+urlpatterns = patterns(
+    'demo.views',
+
+    url(r'^$', 'home', name='home'),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^votes/', include('qhonuskan_votes.urls')),
 )
