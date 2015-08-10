988ec4e6d67bfa6b32cb8d90ec87366bcef33e0e
Kevin Mickey <kwmickey@gmail.com>
2015-08-09 22:42:56 -0700

    update render

--- demo/app/views.py
+++ demo/app/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from app.models import ThreadModel
 from django.template import RequestContext
 
 
 def home(request):
     context_data = {'objects': ThreadModel.objects_with_scores.all()}
-    return render_to_response(
-        'home.html', context_data, context_instance=RequestContext(request))
+    return render(request,'home.html', context_data)
